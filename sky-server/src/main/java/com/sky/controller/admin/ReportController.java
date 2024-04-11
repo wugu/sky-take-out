4b74ae518285c1d9020edee2f34a41e2826c9fb9
肖懿 <2231597633@qq.com>
2024-04-11 22:42:39 +0800

    订单统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -55,5 +56,19 @@ public class ReportController {
         log.info("用户统计:{},{}",begin,end);
         return Result.success(reportService.getUserStatistics(begin,end));
     }
+    /**
+     * 订单统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
+    public Result<OrderReportVO> ordersStatistics(@DateTimeFormat(pattern = "yyy-MM-dd") LocalDate begin,
+                                                  @DateTimeFormat(pattern = "yyy-MM-dd") LocalDate end){
+        log.info("订单统计:{},{}",begin,end);
+        return Result.success(reportService.getOrderStatistics(begin,end));
+    }
+
 
 }
